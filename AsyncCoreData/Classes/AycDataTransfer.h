b6f901b09238d090356c7bddb66b941257c26d07
Roen LolaageMac <zxllf23@163.com>
2020-12-15 16:32:16 +0800

    添加数据迁移方法' ‘ '

---
+++ AsyncCoreData/Classes/AycDataTransfer.h
@@ -0,0 +1,16 @@
+//
+//  AycDataTransfer.h
+//  AsyncCoreData
+//
+//  Created by 罗亮富 on 2020/12/15.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AycDataTransfer : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ AsyncCoreData/Classes/AycDataTransfer.m
@@ -0,0 +1,12 @@
+//
+//  AycDataTransfer.m
+//  AsyncCoreData
+//
+//  Created by 罗亮富 on 2020/12/15.
+//
+
+#import "AycDataTransfer.h"
+
+@implementation AycDataTransfer
+
+@end
